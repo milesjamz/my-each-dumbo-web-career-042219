fe9636f3c27948b010f59a6d48358b2cd727afd3
Miles Marillo <mjm03@hampshire.edu>
2019-04-05 17:30:24 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0
+  newArray = []
+while i < collection.length
+  newArray << yield(collection[i])
+  i += 1 
+  end
+ newArray
 end
